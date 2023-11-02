5b466343434bbd10a0bc87a0904d0b30771361ea
Marcos Martins <contato@marcosmartins.me>
2023-11-02 13:33:43 -0300

    feat: create 'apply_multiple_validators'
    Allow us to apply multiple validators to a single question

--- cz_bitbucket_jira_plugin/validators.py
+++ cz_bitbucket_jira_plugin/validators.py
@@ -1,5 +1,24 @@
+from __future__ import annotations
+
+from typing import Callable
+from typing import List
+
+
 def required_answer_validator(answer):
     if not answer:
         return 'Answer is required.'
 
     return True
+
+
+def apply_multiple_validators(validators: List[Callable]):
+    def apply_validators(answer):
+        for validator in validators:
+            result = validator(answer)
+
+            if not result:
+                return result
+
+        return True
+
+    return apply_validators
