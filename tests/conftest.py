7edc41c6cef21d6bfdcc5172ce810ad436bbf8f7
Marcos Martins <contato@marcosmartins.me>
2024-05-21 20:00:00 -0300

    fix: run tests inside an pre-configured temporary directory
    The tests was not actually passing. When I fixed the tox miss configuration
    I realize that. :clown_face:

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,23 @@
+import os
+
 import pytest
 from commitizen.config import BaseConfig
+from commitizen.config import TomlConfig
 
 
 @pytest.fixture()
 def default_config():
     return BaseConfig()
+
+
+@pytest.fixture
+def setup_tmpdir(tmpdir):
+    with tmpdir.as_cwd():
+        path = tmpdir.mkdir('commitizen')
+        file_path = path.join('.cz.toml')
+        toml_config = TomlConfig(data='', path=file_path)
+        toml_config.init_empty_config_content()
+
+        os.chdir(path)
+
+        yield path
